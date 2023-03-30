785abe7b8db6b31241ace53c9acc6aab502b7c0a
Replit user <>
2023-03-30 20:33:46 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -63,13 +63,10 @@ void* pop(ArrayList * l, int i){
   void *eliminado = NULL;
   eliminado = l->data[i];
 
-
-  
-  for(int k = i ; k < l->size - 1 ; k++)
-  {
-    l->data[k] = l->data[k+1];
-  }
-
+  for(int k = i ; i < l->size - 1 ; k++)
+    {
+      l->data[k] = l->data[k+1];
+    }
     l->size--;
   return eliminado;
  
