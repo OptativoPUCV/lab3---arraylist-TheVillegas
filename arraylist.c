45a288afa26284ee8867127ef3b53e07cbb91016
Replit user <>
2023-03-30 19:16:51 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -41,9 +41,9 @@ void push(ArrayList * l, void * data, int i){
   if(l->size == l->capacity)
   {
     l->capacity *=2;
-    
+    l->data= (void**)realloc(l->data,l->capacity * sizeof(void**));
   }
-  
+  l->size++;
 
 }
 
