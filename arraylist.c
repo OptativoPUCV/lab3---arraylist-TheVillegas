4ef965513276c482b992413b34330358799e66ad
Replit user <>
2023-03-30 15:07:09 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -25,8 +25,11 @@ ArrayList *createList(void) {
 
 void append(ArrayList * l, void * data){
   //agregar dato al final osea en size
+
+  l->capacity *=2;
+  l->data= (void**)realloc(l->data,l->capacity * sizeof(void**));
   l->data[l->size] = data;
-  l->data = (void *)realloc(2*sizeof(void));
+ 
   l->size++;
   
   
