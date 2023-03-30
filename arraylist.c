2faa931e5c1875fe92d921bb014c358f2433e7b5
Replit user <>
2023-03-30 19:41:45 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -45,7 +45,7 @@ void push(ArrayList * l, void * data, int i){
     l->data= (void**)realloc(l->data,l->capacity * sizeof(void**));
   }
 
-  for(int k = l->size ; k < i ; k--)
+  for(int k = l->size ; k > i ; k--)
     {
       l->data[k] = l->data[k-1];
     }
